861665bf8fa56943a4e1176d6bc1c479c92bfda3
t-velev <61898409+de-prox@users.noreply.github.com>
2026-05-03 17:06:21 +0000

    create main terraform cofig file

---
+++ infra/main.tf
@@ -0,0 +1,55 @@
+# 1. Define the Cloud Provider and Region
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# 2. Data Source: Ask an external service what my current public IP address is
+data "http" "myip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+# 3. Security Group: The Virtual Firewall
+resource "aws_security_group" "rds_sg" {
+  name        = "budget-manager-rds-sg"
+  description = "Allow inbound PostgreSQL traffic from my IP"
+
+  # INGRESS (Incoming traffic): Allow Port 5432 ONLY from your exact IP
+  ingress {
+    description = "PostgreSQL from local IP"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  # EGRESS (Outgoing traffic): Allow the database to talk to the internet (needed for updates)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 4. Amazon RDS: The PostgreSQL Database
+resource "aws_db_instance" "budget_db" {
+  identifier             = "aws-budget-manager-db"
+  engine                 = "postgres"
+  engine_version         = "18"
+  instance_class         = "db.t3.micro"    # This is the Free Tier eligible instance
+  allocated_storage      = 20               # 20 GB is the max Free Tier storage
+  storage_type           = "gp2"
+
+  username               = var.db_username
+  password               = var.db_password
+
+  vpc_security_group_ids =[aws_security_group.rds_sg.id]
+  publicly_accessible    = true             # So the local VS Code can reach it
+  skip_final_snapshot    = true             # So we can cleanly delete it without AWS saving backups
+}
+
+# 5. Output: Print the database connection address after it builds
+output "db_endpoint" {
+  value       = aws_db_instance.budget_db.endpoint
+  description = "The connection endpoint for the database"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,10 @@
+variable "db_username" {
+  description = "Database administrator username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database administrator password"
+  type        = string
+  sensitive   = true  # Hides it from Terraform terminal output
+}
